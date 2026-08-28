b7d0b9d2af0cff3d12ba557bd98cc5e6bf303eb8
phazed <wtfux100@yahoo.com>
2026-08-27 22:53:45 -0700

    Load smart lexicon intelligence

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ const ENHANCEMENT_MODULES = [
   "./tool-importer-auto-review.js",
   "./tool-importer-markdown-parser-v2.js",
   "./tool-importer-workflow-fix.js",
-  "./tool-lexicon-ui-v2.js"
+  "./tool-lexicon-ui-v2.js",
+  "./tool-lexicon-intelligence-v1.js"
 ];
 
 async function loadToolModule(path) {
